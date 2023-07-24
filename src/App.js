d492795150b7764512d90789f76efd43f5017502
Ankur Dahiya <113816943+ankur-dahiya@users.noreply.github.com>
2023-07-24 09:51:58 +0530

    Features Added : homepage,loginPage,signupPage,routing

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Home from './pages/Home';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+  Route
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Home></Home>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <LoginPage></LoginPage>
+    ),
+  },
+  {
+    path: "/signup",
+    element: (
+      <SignupPage></SignupPage>
+    ),
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
